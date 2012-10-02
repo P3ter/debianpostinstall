046258387c739e3ab6c90dc3dbc989a65f2f9864
Pedro CADETE <pdr.cadete@gmail.com>
2012-10-02 15:37:07 +0200

    Do not install php5-musql

--- nginxautoinstall.sh
+++ nginxautoinstall.sh
@@ -122,7 +122,7 @@ displayandexec "Update the repositories list" $APT_GET update
 
 # Pre-requis
 displayandexec "Install development tools" $APT_GET install build-essential libpcre3-dev libssl-dev zlib1g-dev
-displayandexec "Install PHP 5" $APT_GET install php5-cli php5-common php5-mysql php5-suhosin php5-fpm php-pear php5-apc php5-gd php5-curl
+displayandexec "Install PHP 5" $APT_GET install php5-common php5-fpm php-pear php5-apc php5-gd php5-curl
 #displayandexec "Install MemCached" $APT_GET install libcache-memcached-perl php5-memcache memcached
 displayandexec "Install Redis" $APT_GET install redis-server php5-redis
 
